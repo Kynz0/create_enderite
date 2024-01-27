b52ca373416ef32536d12ab41ac525b2afbb78ed
kynzo <kynzo.yt@gmail.com>
2024-01-27 01:06:58 +0100

    just to have the github up to date idk

--- src/main/java/net/kynzo/create_enderite/item/ModItemGroups.java
+++ src/main/java/net/kynzo/create_enderite/item/ModItemGroups.java
@@ -1,9 +1,11 @@
 package net.kynzo.create_enderite.item;
 
+
+
 import net.kynzo.create_enderite.CreateEnderite;
 
 public class ModItemGroups {
-    public static final ItemGroup
+
 public static void registerItemGroups() {
     CreateEnderite.LOGGER.info("Registering Item Groups for " + CreateEnderite.MOD_ID);
 }
--- src/main/java/net/kynzo/create_enderite/item/ModItems.java
+++ src/main/java/net/kynzo/create_enderite/item/ModItems.java
@@ -1,10 +1,12 @@
 package net.kynzo.create_enderite.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.kynzo.create_enderite.CreateEnderite;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 
 public class ModItems {
-
-
 
     public static void registerModItems() {
         CreateEnderite.LOGGER.info("Registering Mod Items for " + CreateEnderite.MOD_ID);
